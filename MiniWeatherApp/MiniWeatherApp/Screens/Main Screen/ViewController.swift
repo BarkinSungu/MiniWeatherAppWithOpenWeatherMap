03f01ee305f59417ca90d9b058790460a6db4c3d
BarkinSungu <barkinsungu@gmail.com>
2023-08-29 22:49:25 +0300

    weather shown for real location.

--- MiniWeatherApp/MiniWeatherApp/Screens/Main Screen/ViewController.swift
+++ MiniWeatherApp/MiniWeatherApp/Screens/Main Screen/ViewController.swift
@@ -6,17 +6,34 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    let locationManager = CLLocationManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        WeatherService.shared.fetchWeatherData(latitude: 41.0082, longitude: 28.9784) { result in
-            switch result {
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            locationManager.stopUpdatingLocation()
+            
+            let latitude = location.coordinate.latitude
+            let longitude = location.coordinate.longitude
+            
+            print("latitude: \(latitude)")
+            print("longitude: \(longitude)")
+            
+            WeatherService.shared.fetchWeatherData(latitude: latitude, longitude: longitude) { result in
+                switch result {
                 case .success(let weatherData):
                     DispatchQueue.main.async {
-                        
                         print("Temp: \(weatherData.current.temp)")
                         print("Icon: \(weatherData.current.weather.first?.main)")
                         print("Daily min: \(weatherData.daily.first?.temp.min)")
@@ -26,8 +43,13 @@ class ViewController: UIViewController {
                     }
                 case .failure(let error):
                     print("Error fetching weather data: \(error)")
+                }
             }
         }
+    }
+        
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Location manager error: \(error)")
     }
 
 }
