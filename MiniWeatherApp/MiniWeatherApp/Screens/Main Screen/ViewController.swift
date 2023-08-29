007c1c82ff03682b1efa2aac8a102e7b3a8e41eb
BarkinSungu <barkinsungu@gmail.com>
2023-08-29 15:34:22 +0300

    Project created

---
+++ MiniWeatherApp/MiniWeatherApp/Screens/Main Screen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MiniWeatherApp
+//
+//  Created by Barkın Süngü on 29.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
